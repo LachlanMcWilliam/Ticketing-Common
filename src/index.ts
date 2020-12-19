eec99a54864173d948353b206add7a5d834388f0
Lachlan <mcwilliam01@gmail.com>
2020-12-19 19:43:51 +0000

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Re-export errors
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
@@ -5,6 +6,7 @@ export * from "./errors/not-authorized-error";
 export * from "./errors/not-found-error";
 export * from "./errors/request-validation-error";
 
+// Re-export middleware
 export * from "./middleware/current-user";
 export * from "./middleware/error-handler";
 export * from "./middleware/require-auth";
